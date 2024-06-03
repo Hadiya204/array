811e75872fd4117b0155d8abba5cebc833462976
Hadiya204 <168410223+Hadiya204@users.noreply.github.com>
2024-06-03 15:53:10 +0500

    Add files via upload

---
+++ javascript h/index.js
@@ -0,0 +1,425 @@
+// class activity
+// question:01
+// let age = 20; 
+
+// if (age >= 18) {
+//     console.log('You are eligible to vote.');
+// }
+// else {
+//     console.log('You are not eligible to vote yet.');
+// }
+// // question:02
+// let temperature = 28; 
+
+// if (temperature > 25) {
+//     console.log("It's a hot day!");
+// }
+// else {
+//     console.log("It's a normal day.");
+// }
+// // question:03
+// let student_grade = +prompt("enter your score");
+
+// if (student_grade >= 60) {
+//     console.log('You passed the test!');
+// }
+// else {
+//     console.log('You failed the test.');
+// }
+// // question:04
+// let is_raining = true;
+
+// if (is_raining) {
+//     console.log('Remember to bring an umbrella.');
+// } else {
+//     console.log('Enjoy the sunny weather!');
+// }
+// //question:05
+// let student__age = +prompt("enter your age")
+// if (student__age>18) {
+//     console.log("You are an adult");    
+// }
+// else if(student__age<18){
+//     console.log("You are a minor");
+// }
+// //question:06
+// let number = +prompt("enter the number")
+// if (number > 0) {
+//     console.log("The number is positive");
+//     }
+//  else if(number == 0){
+//     console.log( "The number is negative");
+// }
+// else if (number < 0) {
+//     console.log("The number is negative");
+// }
+// else{
+//     console.log("all number are integers");
+// }
+// // class work
+// // logical operation
+// let score = +prompt("enter your score")
+// if(score >= 80 || score >= 60){
+//     console.log("you passed");
+// }else{
+// console.log("you failed");
+// }
+// let issunny = true;
+// let iswarm = false;
+// if (issunny && iswarm) {
+//     console.log("perfect weather for outdoor activities!");
+// }else{
+//     console.log("may be another day.");
+// }
+// let israining = false;
+// if(israining){
+//     console.log("it's not raining. enjoy the day!");
+// }else{
+//     console.log("don't forget your umbrella");
+// }
+// let weight = +prompt("enter weight")
+// let time = +prompt("enter time")
+// if(weight > 300 && time <6){//weight =244 && time= 4
+//     alert("come to our tryout");
+// }else{
+//     alert("come to our cookout"); 
+// }
+// let weight = +prompt("enter weight")
+// let time = +prompt("enter time")
+// let age = +prompt("enter age")
+// let gender = +prompt("enter gender")
+// if (weight > 300 || time < 6 || age > 17 || gender === "male") {
+//     // 78 > 300 || 5 < 6 || 16 > 17 || male === "male"
+//     alert("come to our tryout");
+// }
+// else{
+//     alert("come to our cookout");
+// }
+// let weight = +prompt("enter weight")
+// let time = +prompt("enter time")
+// if (!(weight > 300 || time < 6 )){
+//     alert("come to our tryout");
+// }
+// else{
+//     alert("come to our cookout");
+// }
+// let x = 10;
+// let y = 5;
+
+// if (x > 5) {
+//     if (y > 2) {
+//         console.log("Both x and y are greater than their respective thresholds.");
+//     } else {
+//         console.log("x is greater than 5, but y is not greater than 2.");
+//     }
+// } else {
+//     console.log("x is not greater than 5, so the inner condition is not checked.");
+// }
+// let temperature = 15;
+// let isSunny = true;
+
+// if(temperature > 25){
+//   console.log("It's hot outside!");
+
+//   if(isSunny){
+//       console.log("Don't forget to wear sunscreen.");
+//   }
+//   else{
+//     console.log("You might still want to wear sunscreen.");
+//   }
+// }
+// else if(temperature >= 15 && temperature <= 25){
+//     console.log("It's a pleasant day.");
+//   if(isSunny){
+//     console.log("Enjoy the sunshine!");
+//   }
+//   else{
+//     console.log("Even if it's cloudy, it's still nice outside.");
+// }
+// }
+// else{
+//     console.log("It's a bit chilly.");
+//     if (isSunny) {
+//         console.log("Don't be fooled by the sun, it's still cool.");
+//     } else {
+//         console.log("It might be cloudy, so bring a jacket just in case.");
+//     }
+// }
+//question no .1
+// let city = +prompt("enter your city name");
+// if (city ="karachi"){
+//     console.log("welcome to city of lights");
+// }
+// else if (city="another city"){
+// console.log("welcome to your city");
+// }
+// else{
+//     console.log("welcome to your city");
+// }
+// //question no .2
+// let gender = +prompt("enter your gender");
+// if (gender = "male"){
+//     console.log("good morning sir");
+// }
+// else if(gender === "female"){
+// console.log("good morning ma'am");
+// }
+// else{
+//     console.log("other");
+// }
+// //question no . 3
+// let signals = +prompt("enter the signal")
+// if (signals = "red"){
+//     console.log("must stop");
+// }
+// else if (signals === "Yellow"){
+// console.log("ready to move");
+// }
+// else if (signals === "Green"){
+//     console.log("move now");
+// }
+// else{
+//     console.log("move on now");
+// }
+// //Arrays
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+// console.log("Welcome to " + city3);
+// let cities = ["atlanta","baltimore","chicago","denver","los Angeles","seatle"];
+// console.log(cities);
+// console.log(cities[0].length);
+// console.log(cities[1].length);
+// let number = [1,4,3,7,8,9,11,43,67,];
+// console.log(number[6].lenght);//undefined
+
+// let info =["hadiya",21,"zari",21,"rimsha",20];
+// console.log(info[1].lenght);//undefined
+// let mixedArray = [1, "Bob", "Now is", true];
+// console.log(mixedArray[2].lenght);
+// //arrays method()
+// let citiesm=["atlanta","baltimore","chicago","denver","los Angeles","seatle"];
+// console.log(citiesm.push("pakistan","india"));
+// console.log(citiesm);
+// citiesm.pop("india");
+// console.log(citiesm);
+// let citieso=["atlanta","baltimore","chicago","denver","los Angeles","seatle"];
+// citieso.push(12,22)
+// console.log(citieso);
+// console.log(citieso.toString());
+//question no.01
+// let futureStudentNames = [];
+// console.log(futureStudentNames);
+// //question no .02
+// let stringsArray = ["monday", "tuesday", "wednesday"];
+// console.log(stringsArray);
+// //question no.03
+// let numbers = ["02", "04", "06"];
+// console.log(numbers);
+// //question no.04
+// let booleanArray = [true, false, true];
+// console.log(booleanArray);
+// //question no.05
+// let mixedArray = [42, "apple", true, null, { name: "John", age: 25 }];
+// console.log(mixedArray);
+// //question no .06
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write("<h2>Educational Qualifications in Pakistan:</h2>");
+// document.write("<ul>");
+// qualifications.forEach(function(qualification) {
+//     document.write("<li>" + qualification + "</li>");
+// });
+// document.write("</ul>");
+// console.log(qualifications);
+// //question no .07
+// const StudentsName = ["micheal" , "john" , "tony"];
+// const studentsScore = [320 , 230 , 480];
+// let total = 500;
+// const percentage1 = (studentsScore[0]/total)*100;
+// const percentage2 = (studentsScore[1]/total)*100;
+// const percentage3 = (studentsScore[2]/total)*100;
+// document.write("score of " + StudentsName[0] + " is " + studentsScore[0] +  ". percentage: "+ percentage1 + "%");
+// document.write("<br> score of " + StudentsName[1] + " is "  + studentsScore[1] + ". percentage: "+ percentage2 + "%");
+// document.write("<br> score of " + StudentsName[2] + " is " + studentsScore[2] + ". percentage: "+ percentage3 + "%");
+// // for loops
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// // console.log("hello world");
+// for (let i = 0;i<10;i++){
+//     console.log("hello world");
+// }
+// for (let i = 0;i>10;i++){
+//     console.log("hello world");
+// }
+// //tables in loop
+// for(let i = 5;i<51;i=i + 5){
+//     console.log("table of 5\n");
+//     console.log(i + "\n");
+// }
+// //reverse table
+// for(let i = 50;i>4;i=i - 5){
+//     console.log(i );
+// }
+for (let i = 0; i < 200; i++) {
+    console.log("hello world");;
+
+}
+for (let i = 0; i < 200; i++) {
+    document.write("hello world");;
+
+}
+for (let i = 14; i < 141; i = i + 14) {
+    console.log(i);;
+}
+for (let i = 14; i < 141; i = i + 14) {
+    document.write(i);;
+}
+
+  
+         let  cleanestCities = ["cheyene" , " santa fe" , "tucson" , "great falls" , "honolulu"];
+         let  cityToCheck    ="great falls";
+          for (var i = 0; i <= 4; i++); {
+            if (cityToCheck === cleanestCities[3]) {
+                 alert("It's one of the cleanest cities");
+                 }
+                 else{
+                    alert(" its not one of the cleanest cities");
+                 }
+                }
+                let  months = ["january" , "february " , "march" , "april" , "may"];
+                let  monthstochecked   ="march";
+                 for (var i = 0; i <= 4; i++); {
+                   if ( monthstochecked === months[2]) {
+                        alert("It's my birthday month");
+                        }
+                        else{
+                           alert(" its not my birthday month");
+                        }
+                       }
+                      
+                     const qualificationofpakistan = ["SSC" , "HSC" , "BSC" , "BS" , "BCOM" , "MS" , "M.PHIL" , "PhD"];
+                       document.write("<h3>QUALIFICATION</h3>");
+                       document.write("<OL>");
+                       for (let i = 0 ; i <= 7; i++) {
+                        document.write("<li><b>" + qualificationofpakistan[i]+ "</b></li>");
+                        }document.write("</OL>");
+
+                        const fruitsofpakistan = ["MANGO" , "APPLE" , "ORANGE" , "BANANA" , "PEAR" , "KIWI" , "CHERRY" , "STRAWBERRY"];
+                        document.write("<h3>FRUITS</h3>");
+                        document.write("<UL>");
+                        for (let i = 0 ; i <= 7; i++) {
+                         document.write("<li><b>" + fruitsofpakistan[i]+ "</b></li>");
+                         }document.write("</UL>");
+                        
+                         // Initialize an array with color names
+    var colors = ["red", "green", "blue", "yellow", "orange"];
+
+    // Display the array elements
+    document.write("<h2>Original Array:</h2>");
+    document.write("<p>" + colors.join(", ") + "</p>");
+
+    // Ask the user what color he/she wants to add to the beginning
+    var newColorBeginning = prompt("Enter a color to add to the beginning:");
+
+    // Add the color to the beginning of the array
+    colors.unshift(newColorBeginning);
+
+    // Display the updated array
+    document.write("<h2>Array After Adding Color to Beginning:</h2>");
+    document.write("<p>" + colors.join(", ") + "</p>");
+
+    // Ask the user what color he/she wants to add to the end
+    var newColorEnd = prompt("Enter a color to add to the end:");
+
+    // Add the color to the end of the array
+    colors.push(newColorEnd);
+
+    // Display the updated array
+    document.write("<h2>Array After Adding Color to End:</h2>");
+    document.write("<p>" + colors.join(", ") + "</p>");
+ // Add two more colors to the beginning of the array
+ colors.unshift("purple", "pink");
+
+ // Display the updated array
+ document.write("<h2>Array After Adding Two More Colors to Beginning:</h2>");
+ document.write("<p>" + colors.join(", ") + "</p>");
+// Delete the first color in the array
+colors.shift();
+
+// Display the updated array
+document.write("<h2>Array After Deleting First Color:</h2>");
+document.write("<p>" + colors.join(", ") + "</p>");
+ // Delete the last color in the array
+ colors.pop();
+
+ // Display the updated array
+ document.write("<h2>Array After Deleting Last Color:</h2>");
+ document.write("<p>" + colors.join(", ") + "</p>");
+// Initialize an array with color names
+var colors = ["red", "green", "blue", "yellow", "orange"];
+
+// Display the array elements
+document.write("<h2>Original Array:</h2>");
+document.write("<p>" + colors.join(", ") + "</p>");
+
+// Ask the user for the index to add the color
+var index = parseInt(prompt("Enter the index where you want to add the color:"));
+
+// Ask the user for the color to add
+var newColor = prompt("Enter the color you want to add:");
+
+// Add the color to the desired position/index
+colors.splice(index, 0, newColor);
+
+// Display the updated array
+document.write("<h2>Array After Adding Color at Index " + index + ":</h2>");
+document.write("<p>" + colors.join(", ") + "</p>");
+
+
+
+
+
+
+
+ // Initialize an array with color names
+ var colors = ["red", "green", "blue", "yellow", "orange"];
+
+ // Display the array elements
+ document.write("<h2>Original Array:</h2>");
+ document.write("<p>" + colors.join(", ") + "</p>");
+
+ // Ask the user for the index to delete the color(s)
+ var indexToDelete = parseInt(prompt("Enter the index from which you want to delete color(s):"));
+
+ // Ask the user for the number of colors to delete
+ var numberOfColorsToDelete = parseInt(prompt("Enter the number of colors you want to delete:"));
+
+ // Delete the color(s) from the array
+ var deletedColors = colors.splice(indexToDelete, numberOfColorsToDelete);
+
+ // Display the updated array
+ document.write("<h2>Array After Deleting " + numberOfColorsToDelete + " Color(s) from Index " + indexToDelete + ":</h2>");
+ document.write("<p>" + colors.join(", ") + "</p>");
+
+ // Display the deleted colors
+ document.write("<h2>Deleted Color(s):</h2>");
+ document.write("<p>" + deletedColors.join(", ") + "</p>");
+ 
